23433394334ee430202fc3c3bee737b6860a34f2
taiful-18 <taifulh902@gmail.com>
2024-04-13 07:45:07 +0600

    for test

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h3 className="text-3xl">this is login page</h3>
+        </div>
+    );
+};
+
+export default Login;
